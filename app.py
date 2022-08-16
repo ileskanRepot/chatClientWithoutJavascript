460b4278e07b6de5f6d13f05d12b2c8fa9c34145
Ileska <ileska@tutanota.com>
2022-08-17 01:09:49 +0300

    Make new possible demo

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 import socket
 
+class WrongAmountExpection(Exception):
+  """Raise for my specific kind of exception"""
+
+class NotMsgOrSenderExpection(Exception):
+  """Raise for my specific kind of exception"""
+
 def parseHeader(headerString):
   headerList = headerString.split("\n")
   firstLineList = headerList[0].split(" ")
@@ -9,14 +15,52 @@ def parseHeader(headerString):
       "TYPE": firstLineList[0],
       "PATH": firstLineList[1],
       "PROTOCOL": firstLineList[2]
+      
     }
   }
-  for header in headerList:
-    headerName = header
-    print(headerName)
-    if header == "REQUEST":
-      headerDic["REQUEST"]["TYPE"] = "GET"
   return headerDic
+
+def renderChats(filename):
+  if ".." in filename or "~" in filename:
+    print ("Error you fool")
+    return "Don't even try"
+  chatList = open("pages/"+filename, "r").read().split("\n")
+  chatString = "<div class=\"SenderMsg\"><p class=\"Sender\">Sender</p><p class=\"Msg\">Msg</p></div>\n"
+  for chat in chatList:
+    if chat:
+      chatChanged = chat.replace("<", "&lt;").replace(">", "&gt;")
+      chatSplit = chatChanged.split(",")
+      chatString += "<div class=\"SenderMsg\"><p class=\"Sender\">" + chatSplit[0] + "</p><p class=\"Msg\">" + chatSplit[1] + "</p></div>\n"
+  return chatString
+
+def parsePostRequest(request,filename):
+  msg, sender = "", ""
+  wholeMsg = request.split("\r\n\r\n")[1]
+  splittedMsg = wholeMsg.split("&")
+  if len(splittedMsg) != 2:
+    raise WrongAmountExpection
+  firstPart = splittedMsg[0].split("=")
+  if firstPart[0] == "sender":
+    sender = firstPart[1].replace("+", " ")
+  elif firstPart[0] == "msg":
+    msg = firstPart[1].replace("+", " ")
+  else:
+    raise NotMsgOrSenderExpection(firstPart[0])
+
+  secondPart = splittedMsg[1].split("=")
+  if secondPart[0] == "sender":
+    sender = secondPart[1].replace("+", " ")
+  elif secondPart[0] == "msg":
+    msg = secondPart[1].replace("+", " ")
+  else:
+    raise NotMsgOrSenderExpection(secondPart[0])
+
+  if sender == "" or msg == "":
+    raise NotMsgOrSenderExpection(". You missed other")
+
+  chatFile = open("pages/"+filename, "a")
+  chatFile.write("\n"+sender+","+msg)
+  return "<p>SUCCESS</p>"
 
 def main():
   # Define socket host and port
@@ -28,36 +72,57 @@ def main():
   server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   server_socket.bind((SERVER_HOST, SERVER_PORT))
   server_socket.listen(1)
+
   print('Listening on port %s ...' % SERVER_PORT)
   
-  while True:  
+  while True:
     # Wait for client connections
     client_connection, client_address = server_socket.accept()
   
+    print("Requested")
     # Get the client request
-    request = client_connection.recv(1024).decode()
+    requestRaw = client_connection.recv(1024)
+    request = requestRaw.decode()
+
+    headers = parseHeader(request)
     
     upperHtml = open("upperHalf.html", "r").read()
     bottomHtml = open("bottomHalf.html", "r").read()
     # chatFile = for i in requset.split("\n") if i 
   
-    headers = parseHeader(request)
     # print("Header Type:",headers["REQUEST"]["TYPE"])
     # print("Header Path:",headers["REQUEST"]["PATH"])
     # print("Header Protocol:",headers["REQUEST"]["PROTOCOL"])
-  
+    # parsePostRequest(request)
     try:
-      response = "HTTP/1.0 200 OK\n\n" + upperHtml + bottomHtml
+      response = "HTTP/1.0 200 OK\n\n"
+      chatFile = headers["REQUEST"]["PATH"].split("/")[1].replace("?","")
+      if chatFile == "favicon.ico":
+        response += "You idiot tried to get my favicon"
+      elif headers["REQUEST"]["TYPE"] == "POST":
+        tmpResponce = parsePostRequest(request,chatFile)
+        response += upperHtml
+        response += renderChats(chatFile)
+        response += tmpResponce
+        response += bottomHtml
+      else:
+        response += upperHtml
+        response += renderChats(headers["REQUEST"]["PATH"].replace("?",""))
+        response += bottomHtml
+    except FileNotFoundError:
+      response = "HTTP/1.0 400 YOU STUPID\n\nFile \"" + headers["REQUEST"]["PATH"].replace("?","") + "\" Not found"
+    except WrongAmountExpection as e:
+      response = "HTTP/1.0 400 YOU STUPID\n\nWrong Amount"
+    except NotMsgOrSenderExpection as e:
+      response = "HTTP/1.0 400 YOU STUPID\n\nNot valid "+str(e)
     except:
-      print("ERR")
-      response = "HTTP/1.0 400 YOU STUPID\n\nERR"
-      print(response)
-    # response = "HTTP/1.0 200 OK\n\n" + upperHtml + chatsHtml + bottomHtml
+      response = "HTTP/1.0 500 ME STUPID\n\nME STUPID"
     client_connection.sendall(response.encode())
     client_connection.close()
-  
+
   # Close socket
   server_socket.close()
+
 
 if __name__ == "__main__":
   main()
